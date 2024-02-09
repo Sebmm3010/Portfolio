e8ce77656c7bce78ea802e2f35b18cfd190e14e7
sebastian <rockmadero@gmail.com>
2024-02-08 20:58:44 -0500

    internacionalizacion

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 import { defineConfig } from "astro/config";
 // Import /static for a static site
-import vercelStatic from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
+  // Internationalization
+  i18n: {
+    defaultLocale: "es",
+    locales: ["es", "en"],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+    
+  },
   // Must be 'static' or 'hybrid'
-  output: "static",
-  adapter: vercelStatic(),
+  output: "server",
+  adapter: vercel(),
 });
