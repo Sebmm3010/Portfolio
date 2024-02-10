9535df036f186c588286eef90f802940e8de71d1
sebastian <rockmadero@gmail.com>
2024-02-09 19:41:34 -0500

    responsive

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,14 @@ import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
   // Internationalization
-  i18n: {
-    defaultLocale: "es",
-    locales: ["es", "en"],
-    routing: {
-      prefixDefaultLocale: false,
-    },
-    
-  },
+  // i18n: {
+  //   defaultLocale: "es",
+  //   locales: ["es", "en"],
+  //   routing: {
+  //     prefixDefaultLocale: false,
+  //   },
+
+  // },
   // Must be 'static' or 'hybrid'
   output: "server",
   adapter: vercel(),
